f8cf807ad5e496a37d3d47bdf6219659914c7e28
Martin Garrido <117180420+garridomartin@users.noreply.github.com>
2023-08-17 11:04:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const PORT = process.env.PORT || 3001;
 
-conn.sync({ alter: true }).then(async () => {
+conn.sync({ alter: true }).then(async () => { 
 //conn.sync({ force: true }).then(async () => {
   //bdd se reinicia con el server↑↑↑
 
